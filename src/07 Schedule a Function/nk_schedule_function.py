ad4eaa37b4b066fd3f2e852e8cb92791830eea7a
NKhaleel <131166318+NKhaleel@users.noreply.github.com>
2023-04-18 16:38:38 +0000

    Updated to include print message

--- src/07 Schedule a Function/nk_schedule_function.py
+++ src/07 Schedule a Function/nk_schedule_function.py
@@ -5,4 +5,6 @@ import sched
 def schedule_function(time_to_run, fun, *fun_inputs):
     s = sched.scheduler(time.time, time.sleep)
     s.enterabs(time_to_run, 1, fun, argument=fun_inputs)
+    print("Will run " + str(fun.__name__) +
+          " at " + str(time.ctime(time_to_run)))
     s.run()
