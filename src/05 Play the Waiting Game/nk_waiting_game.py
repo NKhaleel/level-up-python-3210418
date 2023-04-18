dc4d51c321959c1b8797e1949dfd2eee6d5b0c82
NKhaleel <131166318+NKhaleel@users.noreply.github.com>
2023-04-18 15:39:18 +0000

    Created Waiting Game

---
+++ src/05 Play the Waiting Game/nk_waiting_game.py
@@ -0,0 +1,21 @@
+import random
+from time import time
+
+
+def waiting_game():
+    wait_time = random.randint(1, 4)
+    print("Your target time is " + str(wait_time) + " seconds.")
+    print("---Press Enter to Begin---")
+    input()
+    print("...Press Enter again after " + str(wait_time) + " seconds...")
+    t_start = time()
+    input()
+    t_diff = time() - t_start
+
+    print("Elapsed time: " + str(t_diff) + " seconds")
+    if t_diff > wait_time:
+        return print("(" + str(t_diff - wait_time) + " seconds too slow)")
+    elif t_diff < wait_time:
+        return print("(" + str(wait_time - t_diff) + " seconds too fast)")
+    else:
+        return print("Exactly on target!")
