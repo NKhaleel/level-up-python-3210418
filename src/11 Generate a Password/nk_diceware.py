f0485e1b1e09ec588187a729b6678dc0deb2f865
NKhaleel <131166318+NKhaleel@users.noreply.github.com>
2023-04-19 09:38:46 +0000

    Function for password gen

---
+++ src/11 Generate a Password/nk_diceware.py
@@ -0,0 +1,20 @@
+import random
+
+
+def generate_passphrase(words, wordlist_path="diceware.wordlist.asc"):
+    with open(wordlist_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()[2:7778]
+        num_list = [line.split()[0] for line in lines]
+        word_list = [line.split()[1] for line in lines]
+
+    search = []
+    words_all = []
+
+    for i in range(words):
+        search.append("".join(str(e)
+                      for e in random.choices(range(1, 6), k=5)))
+
+    for code in search:
+        words_all.append(word_list[num_list.index(code)])
+
+    return " ".join(words_all)
